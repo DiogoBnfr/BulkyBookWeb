f3968b504a196c652e4e7b06362416fd705de496
diogobnfr <diogobonofre@gmail.com>
2023-07-26 19:40:54 -0300

    Added Delete() action and view Added notifications to the crud operations

--- BulkyBookWeb/BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/BulkyBookWeb/Controllers/CategoryController.cs
@@ -40,9 +40,85 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["Success"] = "Category created successfully!";
                 return RedirectToAction("Index"); // ("Action", "Controller") if the action isn't inside the current controller    
             }
             return View(obj);
+        }
+
+		// GET
+		public IActionResult Edit(int? id)
+		{
+            if (id == null || id == 0) 
+            {
+                return NotFound();
+            }
+
+			var category = _db.Categories.Find(id);
+			// var category = _db.Categories.FirstOrDefault(u => u.Id == id);
+			// var category = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+			return View(category);
+		}
+
+		// POST
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(Category obj)
+		{
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name!");
+			}
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+                TempData["Success"] = "Category updated successfully!";
+                return RedirectToAction("Index"); // ("Action", "Controller") if the action isn't inside the current controller    
+			}
+			return View(obj);
+		}
+
+        // GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _db.Categories.Find(id);
+            // var category = _db.Categories.FirstOrDefault(u => u.Id == id);
+            // var category = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["Success"] = "Category deleted successfully!";
+            return RedirectToAction("Index");
         }
     }
 }
